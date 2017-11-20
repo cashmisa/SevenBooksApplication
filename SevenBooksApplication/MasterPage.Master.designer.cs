919ab9b6e84d0489307574a596dee06e51d5e94e
Meng Guan <kohmengguan@hotmail.com>
2017-11-20 15:39:27 +0800

    Wow Wonderful colours

--- SevenBooksApplication/MasterPage.Master.designer.cs
+++ SevenBooksApplication/MasterPage.Master.designer.cs
@@ -22,6 +22,15 @@ namespace SevenBooksApplication {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Panel4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel4;
+        
+        /// <summary>
         /// btnHome control.
         /// </summary>
         /// <remarks>
