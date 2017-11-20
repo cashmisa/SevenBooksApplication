c589397d0ea0a5415be4d587557cae7136fe50e4
Ton That Minh Nhat <e0224917@u.nus.edu>
2017-11-20 09:27:34 +0800

    Add project files.

---
+++ SevenBooksApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SevenBooksApplication.Startup))]
+namespace SevenBooksApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
