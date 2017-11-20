d6124f7a4757ba38c8778fcc71d221db8b4eba53
Meng Guan <kohmengguan@hotmail.com>
2017-11-21 01:09:17 +0800

    Revert "some exception for biz logic"
    This reverts commit de1f94236ef1e3f8b6561bd2a5c3f50c4ee5ffa1.

--- SevenBooksApplication/App_Code/BusinessLogic.cs
+++ SevenBooksApplication/App_Code/BusinessLogic.cs
@@ -7,22 +7,22 @@ namespace SevenBooksApplication.App_Code
     public class BusinessLogic
     {
         //BookContext context = new BookContext();
-        public static void AddBook(string title, string categoryName, string isbn, string author, int stock, decimal price)
+        public static void AddBook(string title,string categoryName, string isbn,string author, int stock , decimal price)
         {
             using (BookContext context = new BookContext())
             {
                 Book book = new Book
                 {
-                    Title = title,
-                    ISBN = isbn,
-                    Author = author,
-                    Stock = stock,
-                    CategoryID = getCategoryID(categoryName),
-                    Price = price
-                };
+                 Title = title,
+                 ISBN = isbn,
+                Author = author,
+                Stock = stock,
+                CategoryID = getCategoryID(categoryName),
+                Price = price
+            };
                 context.Books.Add(book);
                 context.SaveChanges();
-
+                
             }
         }
         public static void UpdateBook(int BookID, string Title, int CategoryID, string ISBN, string Author, int Stock, decimal Price)
@@ -64,76 +64,60 @@ namespace SevenBooksApplication.App_Code
         }
         public static Book SearchBookByISBN(string ISBN)
         {
-            try
+            using (BookContext context = new BookContext())
             {
-                using (BookContext context = new BookContext())
-                {
-                    return context.Books.Where(x => x.ISBN == ISBN).ToList<Book>()[0];
-                }
+                return context.Books.Where(x=>x.ISBN == ISBN).ToList<Book>()[0];
             }
-            catch (System.ArgumentOutOfRangeException)
+        }
+        public static int getCategoryID(string category)
+        {
+            using (BookContext context = new BookContext())
             {
-                Book noBookNull = null;
-                return noBookNull;
+
+                Category c= context.Categories.Where(x => x.Name == category).ToList<Category>()[0];
+               
+                return c.CategoryID;
+
             }
-
         }
-    
-    public static int getCategoryID(string category)
-    {
-            try
+        public static List<Book> SearchBookByCategory(string category)
+        {
+            using (BookContext context = new BookContext())
             {
-                using (BookContext context = new BookContext())
-                {
-
-                    Category c = context.Categories.Where(x => x.Name == category).ToList<Category>()[0];
-
-                    return c.CategoryID;
-
-                }
-            }catch (System.ArgumentOutOfRangeException)
-            {
-                return 0;
+                int categoryID = getCategoryID(category);
+                return context.Books.Where(x => x.CategoryID == categoryID).ToList<Book>();
+                
             }
-    }
-    public static List<Book> SearchBookByCategory(string category)
-    {
-        using (BookContext context = new BookContext())
+        }
+        public static List<Order> SearchAllOrder()
         {
-            int categoryID = getCategoryID(category);
-            return context.Books.Where(x => x.CategoryID == categoryID).ToList<Book>();
-
+            using (BookContext context = new BookContext())
+            {
+                return context.Orders.ToList<Order>();
+            }
+        }
+        public static List<Order> SearchOrderByUserID(string UserID)
+        {
+            using (BookContext context = new BookContext())
+            {
+                return context.Orders.Where(x => x.UserID == UserID).ToList<Order>();
+            }
+        }
+        public static Order SearchByOrderID(int OrderID)
+        {
+            using (BookContext context = new BookContext())
+            {
+                return context.Orders.Where(x => x.OrderID == OrderID).First();
+            }
+        }
+        public static void UpdateOrder(int OrderID,string OrderStatus)
+        {
+            using (BookContext context = new BookContext())
+            {
+                Order order = context.Orders.Where(x => x.OrderID == OrderID).First();
+                order.OrderStatus = OrderStatus;
+                context.SaveChanges();
+            }
         }
     }
-    public static List<Order> SearchAllOrder()
-    {
-        using (BookContext context = new BookContext())
-        {
-            return context.Orders.ToList<Order>();
-        }
-    }
-    public static List<Order> SearchOrderByUserID(string UserID)
-    {
-        using (BookContext context = new BookContext())
-        {
-            return context.Orders.Where(x => x.UserID == UserID).ToList<Order>();
-        }
-    }
-    public static Order SearchByOrderID(int OrderID)
-    {
-        using (BookContext context = new BookContext())
-        {
-            return context.Orders.Where(x => x.OrderID == OrderID).First();
-        }
-    }
-    public static void UpdateOrder(int OrderID, string OrderStatus)
-    {
-        using (BookContext context = new BookContext())
-        {
-            Order order = context.Orders.Where(x => x.OrderID == OrderID).First();
-            order.OrderStatus = OrderStatus;
-            context.SaveChanges();
-        }
-    }
-}
 }
