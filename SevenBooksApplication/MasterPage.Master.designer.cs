96df706d5c3980ea9f338f9633b95cba80fc6a5f
Xiaowen <Xiaowen@DESKTOP-25VFDDQ>
2017-11-21 11:58:17 +0800

    updated

--- SevenBooksApplication/MasterPage.Master.designer.cs
+++ SevenBooksApplication/MasterPage.Master.designer.cs
@@ -103,13 +103,13 @@ namespace SevenBooksApplication {
         protected global::System.Web.UI.WebControls.Button btnManageBook;
         
         /// <summary>
-        /// btnManageCustomer control.
+        /// btnManageOrder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnManageCustomer;
+        protected global::System.Web.UI.WebControls.Button btnManageOrder;
         
         /// <summary>
         /// btnManageDiscount control.
