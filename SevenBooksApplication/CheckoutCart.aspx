0c1fc498940ab205b2040a1573fa99a52a8a2277
CK Tan <tanck105@hotmail.com>
2017-11-21 12:15:48 +0800

    CheckoutCart

--- SevenBooksApplication/CheckoutCart.aspx
+++ SevenBooksApplication/CheckoutCart.aspx
@@ -1,3 +1,34 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="CheckoutCart.aspx.cs" Inherits="SevenBooksApplication.CheckoutCart" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Thanks for your orders. Below is your order details:"></asp:Label><br />
+    <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" HorizontalAlign="Center" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowDataBound="GridView1_RowDataBound" PageSize="3">
+        <Columns>
+            <asp:TemplateField HeaderText="Book Title">
+                <ItemTemplate>
+                    <asp:Image ID="Image1" runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Quantity">
+                <ItemTemplate>
+                    <asp:Label ID="lblQty" runat="server" Text="Label"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Unit Price">
+                <ItemTemplate>
+                    <asp:Label ID="lblPrice" runat="server" Text="Label"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Total Price">
+                <ItemTemplate>
+                    <asp:Label ID="lblTotalPrice" runat="server" Text="Label"></asp:Label><br />
+                    <asp:Label ID="lblDiscount" runat="server" Text="Label" Visible="False"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <PagerSettings Position="TopAndBottom" />
+        <PagerStyle HorizontalAlign="Left" VerticalAlign="Middle" />
+    </asp:GridView>
+    <p>
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/MyAccount.aspx">Click here </asp:HyperLink>  to view all order history
+    </p>
 </asp:Content>
--- SevenBooksApplication/CheckoutCart.aspx.cs
+++ SevenBooksApplication/CheckoutCart.aspx.cs
@@ -4,14 +4,79 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SevenBooksApplication.App_Code;
+using SevenBooksApplication.Models;
 
 namespace SevenBooksApplication
 {
     public partial class CheckoutCart : System.Web.UI.Page
     {
+        enum ColumnIndex { Title, Quantity, Price, TotalPrice}
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                List<Book> bookList = Session["cartList"] as List<Book>;
+                Dictionary<string, int> bookCounts = bookList.GroupBy(x => x.ISBN)
+                                                    .ToDictionary(k => k.Key, v => v.Count());
 
+                List<GridDisplayData> dataList = new List<GridDisplayData>();
+
+                foreach(var key in bookCounts.Keys)
+                {
+                    dataList.Add(new GridDisplayData { ISBN = key, Quantity = bookCounts[key] });
+                }
+
+                GridView1.DataSource = dataList;
+                GridView1.DataBind();
+            }
         }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow)
+            {
+                GridDisplayData data = e.Row.DataItem as GridDisplayData;
+
+                Image img = e.Row.Cells[(int)ColumnIndex.Title].FindControl("Image1") as Image;
+                img.ImageUrl = string.Format("~/image/{0}.jpg", data.ISBN);
+                Book book = BusinessLogic.SearchBookByISBN(data.ISBN);
+                img.AlternateText = book.Title;
+
+                Label lblQty = e.Row.Cells[(int)ColumnIndex.Quantity].FindControl("lblQty") as Label;
+                lblQty.Text = data.Quantity.ToString();
+
+                Label lblPrice = e.Row.Cells[(int)ColumnIndex.Price].FindControl("lblPrice") as Label;
+                lblPrice.Text = book.Price.ToString();
+
+                Label lblTotalPrice = e.Row.Cells[(int)ColumnIndex.TotalPrice].FindControl("lblTotalPrice") as Label;                
+                decimal currentDiscount = BusinessLogic.GetCurrentDiscount();
+                decimal totalPrice = book.Price * data.Quantity * (1 - currentDiscount);
+                lblTotalPrice.Text = totalPrice.ToString();
+
+                decimal discountAmt = book.Price * data.Quantity * currentDiscount;
+
+                if (currentDiscount != 0)
+                {
+                    Label lblDiscount = e.Row.Cells[(int)ColumnIndex.TotalPrice].FindControl("lblDiscount") as Label;
+                    string msg = string.Format("(You saved {0:c})", discountAmt);
+                    lblDiscount.Visible = true;
+                    lblDiscount.ForeColor = System.Drawing.Color.Red;
+                    lblDiscount.Font.Italic = true;
+                }
+            }
+        }
+    }
+
+    public class GridDisplayData
+    {
+        public string ISBN { get; set; }
+        public int Quantity { get; set; }
     }
 }
