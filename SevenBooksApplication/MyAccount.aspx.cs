ae47d6a6bb6f9b662d78362c99d0a79f7d8a5348
CK Tan <tanck105@hotmail.com>
2017-11-21 10:44:18 +0800

    MyAccount + CartDetail

--- SevenBooksApplication/MyAccount.aspx.cs
+++ SevenBooksApplication/MyAccount.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SevenBooksApplication.App_Code;
+using SevenBooksApplication.Models;
+using System.Security.Principal;
+using System.Web.Security;
 
 namespace SevenBooksApplication
 {
@@ -11,7 +15,34 @@ namespace SevenBooksApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                MembershipUser currentUser = Membership.GetUser();
+                Guid currentUserId = (Guid) currentUser.ProviderUserKey;
 
+                List<Order> orderHistory = BusinessLogic.GetOrderHistory(currentUserId.ToString());
+                if(orderHistory.Count == 0)
+                {
+                    this.MultiView1.ActiveViewIndex = 0;
+                }
+                else
+                {
+                    this.MultiView1.ActiveViewIndex = 1;
+                    this.gvOrderHistory.DataSource = BusinessLogic.GetOrderHistory(currentUserId.ToString());
+                    this.gvOrderHistory.DataBind();
+                }
+            }
+        }
+
+        protected void gvOrderHistory_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Order order = e.Row.DataItem as Order;
+                Book book = BusinessLogic.SearchBookByBookId(order.BookID);
+
+                ((Label)e.Row.FindControl("lblTitle")).Text = book.Title;
+            }
         }
     }
 }
--- SevenBooksApplication/MyAccount.aspx.designer.cs
+++ SevenBooksApplication/MyAccount.aspx.designer.cs
@@ -11,5 +11,50 @@ namespace SevenBooksApplication {
     
     
     public partial class MyAccount {
+        
+        /// <summary>
+        /// MultiView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView MultiView1;
+        
+        /// <summary>
+        /// View1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// View2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View View2;
+        
+        /// <summary>
+        /// gvOrderHistory control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvOrderHistory;
     }
 }
